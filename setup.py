ce5c0998429827ac9549612a28725523c2057916
poke30744 <poke30744@gmail.com>
2021-11-13 22:16:11 +0900

    exclude "tests" folder; add build number

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
-import setuptools
+import setuptools, os
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+build_number = os.getenv('BUILD_NUMBER') or '0'
+
 setuptools.setup(
     name="tscutter",
-    version="0.0.9",
+    version=f"0.1.{build_number}",
     author="poke30744",
     author_email="poke30744@gmail.com",
     description="Cut MEPG TS files into clips",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=['tests',]),
     install_requires=[
         'pydub',
         'tqdm',
@@ -24,5 +26,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.8',
 )
